8714f3c6e061ecb4791997e4c39b0f7d690cf3ca
Adrián Goia <iasgoi@alu.edu.gva.es>
2025-10-25 11:24:17 +0200

    u

--- src/ut02/bloque4/Temperatura.java
+++ src/ut02/bloque4/Temperatura.java
@@ -2,17 +2,14 @@ package ut02.bloque4;
 
 public class Temperatura {
 
-    // Convierte grados Celsius a Farenheit
     public static double celsiusToFarenheit(double celsius) {
         return (1.8 * celsius) + 32;
     }
 
-    // Convierte grados Farenheit a Celsius
     public static double farenheitToCelsius(double farenheit) {
         return (farenheit - 32) / 1.8;
     }
 
-    // Prueba de los métodos
     public static void main(String[] args) {
         double celsius = 20;
         double farenheit = 68;
